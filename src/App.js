f9e6450618e4766c3d39db3973a12faeefecd541
surenderhsr44 <suenderhsr44@gmail.com>
2018-08-08 11:41:38 +0530

    Add hero image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './component/Header'
+import Header from './component/Header';
+import Hero from './component/Hero';
 
 class App extends Component {
   render() {
     return (
       <div >
         <Header />
+        <Hero />
       </div>
     );
   }
