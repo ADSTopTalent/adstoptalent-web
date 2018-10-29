8578dc86d729d6da38a8bf3120089400dd5216b1
Mukulsharmahsr5 <mukulsharmahsr5@gmail.com>
2018-10-29 17:11:10 +0530

    some files updated

--- src/JoinUs.js
+++ src/JoinUs.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
+import './Transition.css';
 import TextField from '@material-ui/core/TextField';
 import { BrowserRouter as Router, Route, Link ,withRouter } from "react-router-dom";
 import MyAccount from './MyAccount';
@@ -11,15 +8,27 @@ import MyAccount from './MyAccount';
 class JoinUs extends Component {
   constructor(props) {
     super(props);
-    this.state = { detail : null}
+    this.state = { detail : null, firstname: '', lastname:'', emailId:''};
+  }
+
+  onChangeFirstName = (event) => {
+    this.setState({firstname: event.target.value});
+  }
+
+  onChangeLastName = (event) => {
+    this.setState({lastname: event.target.value});
+  }
+
+  onChangeEmailId = (event) => {
+    this.setState({emailId: event.target.value});
   }
 
   onClickButton = () => {
-    const firstname = this.refs.firstname.value;
-    const lastname = this.refs.lastname.value;
-    const emailId = this.refs.emailId.value;
+  const firstname = this.state.firstname;
+  const lastname = this.state.lastname;
+  const emailId = this.state.emailId;
 
-    console.log('emailId: ' +emailId+ 'firstName:' +firstname+ 'lastName:'+lastname);
+    console.log('emailId: ' +emailId+ 'firstname:' +firstname+ 'lastname:'+lastname);
     fetch('http://192.168.0.123:3000/register', {
       method: 'POST',
         headers: {
@@ -45,15 +54,21 @@ class JoinUs extends Component {
       <div>
         <div className="signupform">
 
-        <h1 className="joinus">SIGN IN</h1>
+        <h1 className="joinus">SIGN UP</h1>
 
-        <TextField className="signuptextfield" id="outlined-name" label="FIRST NAME"  ref="firstname" margin="normal" variant="outlined" /><br />
+        <TextField className="signuptextfield" value={this.state.firstname} 
+        onChange={this.onChangeFirstName} minlength="2" label="FIRST NAME" 
+        margin="normal" variant="outlined" /><br />
 
-        <TextField className="signuptextfield" id="outlined-name" label="LAST NAME"  ref="lastname" margin="normal" variant="outlined" /><br />
+        <TextField className="signuptextfield" value={this.state.lastname} 
+        onChange={this.onChangeLastName} minlength="2" label="LAST NAME" 
+        margin="normal" variant="outlined" /><br />
 
-        <TextField className="signuptextfield" id="outlined-name" label="EMAIL"  ref="emailId" margin="normal" variant="outlined" /><br />
+        <TextField className="signuptextfield" value={this.state.emailId} 
+        onChange={this.onChangeEmailId} type="email" label="EMAIL" 
+        margin="normal" variant="outlined" /><br />
 
-        <input className="signupcheckbox" type="checkbox" /> Remember Me<br/><br /><br />
+        <input className="signupcheckbox" type="checkbox" /> Remember Me<br/><br />
 
         <button className="signupbtn" onClick={this.onClickButton}>CREATE ACCOUNT</button>
 
