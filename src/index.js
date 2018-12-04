99d957f4e3b3de510259dfd64ebed188636c30dd
surenderhsr44 <40272615+surenderhsr44@users.noreply.github.com>
2018-12-04 13:55:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MyAccount from './MyAccount';
-import Signup2 from './Signup2';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Link ,withRouter } from "react-router-dom";
 import verify from './verify';
 
-ReactDOM.render(
-
-	<Router>	
-		<div>
-			<Route path='/MyAccount' component={MyAccount} />
-			<Route path='/Signup2' component={Signup2} />
-			<Route path='/verify/:id' component={verify} />
-		</div>
-	</Router>
-
-, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
