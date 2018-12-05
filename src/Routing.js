1552817f193f21de828b059475d1e0592ce54c41
surenderhsr44 <suenderhsr44@gmail.com>
2018-12-05 10:47:51 +0530

    routing

---
+++ src/Routing.js
@@ -0,0 +1,31 @@
+import React , {Component} from 'react';
+import MyAccount from './MyAccount';
+import Signup2 from './Signup2';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Link ,withRouter } from "react-router-dom";
+import verify from './verify';
+import JoinUs from './JoinUs';
+import App from './App';
+import MyAccountComponent from './MyAccountComponent';
+
+class Routing extends Component{
+	render(){
+	return(
+		<div>
+		<Router>	
+			<div>
+
+				<Route exact path='/' component={App} />
+				<Route path='/MyAccountComponent' component={MyAccountComponent} />
+				<Route path='/JoinUs' component={JoinUs} />
+				<Route path='/MyAccount' component={MyAccount} />
+				<Route path='/Signup2' component={Signup2} />
+				<Route path='/verify/:id' component={verify} />
+			</div>
+		</Router>
+
+		</div>
+		)
+}
+}
+export default Routing;
