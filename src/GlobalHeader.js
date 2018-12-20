2be5911971a2c9fb98a7a383023b848cbacae466
Sanjeev sharma <40255512+Sanjeevsharmaads@users.noreply.github.com>
2018-12-20 16:00:45 +0530

    Update GlobalHeader.js

--- src/GlobalHeader.js
+++ src/GlobalHeader.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 import './Header.css';
 import Socialicons from './Socialicons';
 import LoginModal from './LoginModal';
-<<<<<<< HEAD
 import JoinUs from './JoinUs';
 import Thanks from './Thanks';
 import Verify from './Verify';
-=======
->>>>>>> 6587523f99e3a94b7a4a17edfd3daec58e745cf5
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class GlobalHeader extends Component {
@@ -17,12 +14,9 @@ class GlobalHeader extends Component {
       	<div className="Gheader"> 
 	      <Socialicons />
 	      <LoginModal />
-<<<<<<< HEAD
 	      <Route path ='/JoinUs' component={JoinUs} />
-        <Route path ='/Thanks' component={Thanks} />
-        <Route path = '/verify/:id' component={Verify}
-=======
->>>>>>> 6587523f99e3a94b7a4a17edfd3daec58e745cf5
+              <Route path ='/Thanks' component={Thanks} />
+       	      <Route path = '/verify/:id' component={Verify} />
 	    </div>
       </Router>
     );
